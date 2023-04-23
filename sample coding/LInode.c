6253de0b5e567ee0546614270ae162f277634711
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2023-04-23 11:30:58 +0530

    u1

---
+++ sample coding/LInode.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+     int data;
+     struct node *link;
+};
+typedef struct node n;
+int main(){
+       n *head=(n *)malloc(sizeof(n));
+       head -> data= 45;
+       head -> link= NULL;
+       
+       n *c1 =(n *)malloc(sizeof(n));
+       c1 -> data=98;
+       c1 -> link= NULL;
+       head ->link=c1;
+       
+       n *c2 =(n *)malloc(sizeof(n));
+       c2 -> data=3;
+       c2 -> link=NULL;
+       c1 -> link=c2;
+
+
+return 0;
+}
