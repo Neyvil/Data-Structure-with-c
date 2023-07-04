51c75850e1956e4d96469bb78b05b378456abf08
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2023-07-04 21:31:46 +0530

    edit

--- practice/Stack/application/postfix.c
+++ practice/Stack/application/postfix.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #define size 100
 typedef struct node{
     char stack[size];
+    char postfix[size];
     int top;
 }ns;
 int isfull(ns *r){
@@ -29,9 +31,47 @@ void push(ns *s,char symbol){
     s->stack[s->top]=symbol;
 }
 char pop(ns *s){
-    
+    if(isempty(s)){
+        printf("Stack Underflow");
+    }
+    char c=s->stack[s->top];
+    s->top--;
+    return c;
+}
+int calculate(ns* n){
+    char a;
+    int i;
+    for(i=0;i<strlen(n->postfix);i++){
+        a=n->postfix[i];
+        if(a>='0' && a<='9'){
+            push(n,a-'0');
+        }
+        else{
+            int a1=pop(n);
+            int a2=pop(n);
+            switch(a){
+                case '+':
+                    push(n,a2+a1);
+                    break;
+                case '-':
+                    push(n,a2-a1);
+                    break;
+                case '*':
+                    push(n,a2*a1);
+
+            }
+        }
+    }
+    return pop(n);
+
 }
 int main(){
-    
+    ns s;
+    s.top=-1;
+    printf("Enter postfix expression: ");
+    scanf("%s",s.postfix);
+    for(int i=0;i<strlen(s.postfix);i++)
+         printf("%c",s.postfix[i]);
+    printf("\nThe answer is:%d",calculate(&s));
 return 0;
 }
