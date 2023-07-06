20b110d4952f42141c1afab550769b85111d0c55
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2023-07-06 13:46:22 +0530

    edit

--- practice/sorting/quick_sort.c
+++ practice/sorting/quick_sort.c
@@ -3,11 +3,11 @@
 void swap(int *a,int *b){
     int temp=*a;
     *a=*b;
-    *b=*a;
+    *b=temp;
 }
 int partition(int arr[],int low,int high){
     int pivotpoint=arr[high];
-    int i=low-1;
+    int i=(low-1);
     for(int j=low;j<=high-1;j++){
         if(arr[j]<pivotpoint){
             i++;
@@ -17,7 +17,33 @@ int partition(int arr[],int low,int high){
     swap(&arr[i+1],&arr[high]);
     return (i+1);
 }
-int main(){
+void quicksort(int arr[],int low,int high){
+    if(low<high){
+        int pivotindex=partition(arr,low,high);
+        quicksort(arr,low,pivotindex-1);
+        quicksort(arr,pivotindex+1,high);
 
-return 0;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver code
+int main() {
+    int arr[] = {3,9,1,12,4,90,10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, size);
+
+    quicksort(arr, 0, size - 1);
+
+    printf("Sorted array: ");
+    printArray(arr, size);
+
+    return 0;
 }
